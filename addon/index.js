b99492f154e71514b7460d5054ecaeae3a30fe98
fengb <contact@fengb.info>
2016-02-25 17:02:47 -0600

    Allow for easier configuration

---
+++ addon/index.js
@@ -0,0 +1,4 @@
+export { default } from 'ember-ambitious-forms/services/ambitious-forms'
+
+export { default as AFField } from 'ember-ambitious-forms/components/af-field'
+export { default as AFForm } from 'ember-ambitious-forms/components/af-form'
--- addon/services/ambitious-forms.js
+++ addon/services/ambitious-forms.js
@@ -1,5 +1,28 @@
 import Ember from 'ember'
 
+import AFField from '../components/af-field'
+
+import i18n from '../mixins/i18n'
+import loc from '../mixins/loc'
+import restless from '../mixins/restless'
+import validations from '../mixins/validations'
+
+const AF_FIELD_MIXINS = { i18n, loc, restless, validations }
+
 export default Ember.Service.extend({
-  defaultPrompt: 'Select'
+  defaultPrompt: 'Select',
+  fieldPlugins: Ember.A(),
+
+  _loadFieldPlugins: Ember.on('init', function () {
+    // TODO: localize plugins instead of tossing them onto the base class
+    for (let plugin of this.get('fieldPlugins')) {
+      if (plugin instanceof Ember.Mixin) {
+        AFField.reopen(plugin)
+      } else if (AF_FIELD_MIXINS[plugin]) {
+        AFField.reopen(AF_FIELD_MIXINS[plugin])
+      } else {
+        Ember.warn(`Not a valid plugin: ${plugin}`)
+      }
+    }
+  })
 })
