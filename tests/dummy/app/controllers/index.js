7d89b2c808ffd056d8011b3a2a8e4e23885348b0
fengb <contact@fengb.info>
2016-04-14 16:34:29 -0500

    Updated example code

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,10 +1,6 @@
 import Ember from 'ember'
 
 export default Ember.Controller.extend({
-  init () {
-    this._super()
-  },
-
   formParams: Ember.computed(function () {
     return {}
   }),
@@ -12,12 +8,12 @@ export default Ember.Controller.extend({
   boolOpts: Ember.A([true, false]),
 
   actions: {
-    formSubmit (params) {
-      console.log('Success!', JSON.stringify(params))
+    formSubmit (afForm) {
+      console.log('Success!', afForm)
     },
 
-    formError (params) {
-      console.warn('Failure!', JSON.stringify(params))
+    formError (afForm) {
+      console.warn('Failure!', afForm)
     }
   }
 })
