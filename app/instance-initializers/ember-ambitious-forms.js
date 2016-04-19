ce3c039d21e6e656eb14382be8724c4cb74b16db
fengb <contact@fengb.info>
2016-04-19 16:56:01 -0500

    Load config from ENV

--- app/instance-initializers/ember-ambitious-forms.js
+++ app/instance-initializers/ember-ambitious-forms.js
@@ -1,16 +1,26 @@
+import Ember from 'ember'
+import config from '../config/environment'
+
 export function initialize (appInstance) {
   let emberAmbitiousForms = appInstance.lookup('service:ember-ambitious-forms')
-  emberAmbitiousForms.configure((config) => {
-    if (appInstance.hasRegistration('service:i18n')) {
-      config.fieldPlugins.push('i18n')
-    }
-    if (appInstance.hasRegistration('service:validations')) {
-      config.fieldPlugins.push('validations')
-    }
-    if ((typeof RESTless != 'undefined') && (RESTless instanceof Ember.Namespace)) {
-      config.fieldPlugins.push('restless')
-    }
-  })
+  if (config['ember-ambitious-forms']) {
+    emberAmbitiousForms.configure(config['ember-ambitious-forms'])
+  }
+
+  if (!Ember.get(config, 'ember-ambitious-forms.fieldPlugins')) {
+    // Detect plugins if they are not defined
+    emberAmbitiousForms.configure((eaf) => {
+      if (appInstance.hasRegistration('service:i18n')) {
+        eaf.fieldPlugins.push('i18n')
+      }
+      if (appInstance.hasRegistration('service:validations')) {
+        eaf.fieldPlugins.push('validations')
+      }
+      if ((typeof RESTless != 'undefined') && (RESTless instanceof Ember.Namespace)) {
+        eaf.fieldPlugins.push('restless')
+      }
+    })
+  }
 }
 
 export default {
