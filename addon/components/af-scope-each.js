1239d7294c747271c284d0f3af7a52610d314708
fengb <contact@fengb.info>
2016-05-25 14:37:25 -0500

    Added classNames to af-scope and af-scope-each

--- addon/components/af-scope-each.js
+++ addon/components/af-scope-each.js
@@ -1,6 +1,7 @@
 import Ember from 'ember'
 
 export default Ember.Component.extend({
+  classNames: 'af-scope-each'
 }).reopenClass({
   positionalParams: ['scopes']
 })
--- addon/components/af-scope.js
+++ addon/components/af-scope.js
@@ -1,6 +1,7 @@
 import Ember from 'ember'
 
 export default Ember.Component.extend({
+  classNames: 'af-scope'
 }).reopenClass({
   positionalParams: ['scope']
 })
