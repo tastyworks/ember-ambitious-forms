622e17b903f1d6a0dac5f5eabe9a6382c76cd314
fengb <contact@fengb.info>
2016-06-24 15:42:09 -0500

    Removed unnecessary click handler

--- addon/components/af-checkbox.js
+++ addon/components/af-checkbox.js
@@ -4,11 +4,5 @@ export default Ember.Component.extend({
   layoutName: 'ember-ambitious-forms@components/af-checkbox',
   tagName: 'label',
   classNames: 'af-checkbox',
-  classNameBindings: 'value:checked:unchecked',
-
-  click (event) {
-    if (event.target.type === 'checkbox') {
-      this.sendAction('action', event.target.checked)
-    }
-  }
+  classNameBindings: 'value:checked:unchecked'
 })
