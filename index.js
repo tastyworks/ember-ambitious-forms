ca21be0a99511fead524bec51865385d3222d42f
Kevin Tung <kekekevin@gmail.com>
2019-06-27 09:52:26 -0500

    fix _super call

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 module.exports = {
   name: 'ember-ambitious-forms',
   init: function (app) {
-    this._super(...arguments)
+    this._super.init && this._super.init.apply(this, arguments);
     this.options = this.options || {}
     this.options.babel = this.options.babel || {}
     this.options.babel.optional = this.options.babel.optional || []
