56e3f716139a5818593c20debab313a700565d31
Kevin Tung <kekekevin@gmail.com>
2019-10-04 12:31:09 -0500

    ember 2.13

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,15 +1,8 @@
-/* jshint node:true */
-/* global require, module */
-var EmberAddon = require('ember-cli/lib/broccoli/ember-addon')
+/* eslint-env node  */
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon')
 
 module.exports = function (defaults) {
   var app = new EmberAddon(defaults, {
-    babel: {
-      includePolyfill: true,
-      optional: [
-        'es7.decorators'
-      ]
-    }
   })
 
   /*
--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-/* jshint node: true */
+/* eslint-env node */
 'use strict'
 
 module.exports = {
-  name: 'ember-ambitious-forms',
-  init: function (app) {
-    this._super.init && this._super.init.apply(this, arguments);
-    this.options = this.options || {}
-    this.options.babel = this.options.babel || {}
-    this.options.babel.optional = this.options.babel.optional || []
-
-    if (this.options.babel.optional.indexOf('es7.decorators') === -1) {
-      this.options.babel.optional.push('es7.decorators')
-    }
-  }
+  name: 'ember-ambitious-forms'
 }
